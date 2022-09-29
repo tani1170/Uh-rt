a6711e4edf6836b3bfdf7dc7d7ce70d86def0307
Your Name <you@example.com>
2022-09-29 23:44:38 +0200

    done

--- src/main.js
+++ src/main.js
@@ -1,1 +1,114 @@
 import "./style.css";
+
+const event = document.querySelector(
+  "#event_btn"
+);
+
+const dropEvent =
+  document.querySelector("#drop_event");
+
+const torsdag =
+  document.querySelector("#tors");
+const fredag =
+  document.querySelector("#fre");
+const lordag =
+  document.querySelector("#lor");
+const torscal = document.querySelector(
+  "#tors_cal"
+);
+const frecal =
+  document.querySelector("#fre_cal");
+const lorcal =
+  document.querySelector("#lor_cal");
+
+event.addEventListener(
+  "click",
+  lukDrop
+);
+
+function lukDrop() {
+  dropEvent.classList.remove("hidden");
+}
+
+torsdag.addEventListener(
+  "click",
+  openTors
+);
+
+fredag.addEventListener(
+  "click",
+  openFre
+);
+
+lordag.addEventListener(
+  "click",
+  openLor
+);
+
+function openTors() {
+  torscal.classList.remove("hidden");
+  frecal.classList.add("hidden");
+  lorcal.classList.add("hidden");
+}
+
+function openFre() {
+  torscal.classList.add("hidden");
+  frecal.classList.remove("hidden");
+  lorcal.classList.add("hidden");
+}
+
+function openLor() {
+  torscal.classList.add("hidden");
+  frecal.classList.add("hidden");
+  lorcal.classList.remove("hidden");
+}
+
+var countDownDate = new Date(
+  "Jan 5, 2023 15:37:25"
+).getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(
+    distance / (1000 * 60 * 60 * 24)
+  );
+  var hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) /
+      (1000 * 60 * 60)
+  );
+  var minutes = Math.floor(
+    (distance % (1000 * 60 * 60)) /
+      (1000 * 60)
+  );
+  var seconds = Math.floor(
+    (distance % (1000 * 60)) / 1000
+  );
+
+  // Display the result in the element with id="demo"
+  document.getElementById(
+    "demo"
+  ).innerHTML =
+    days +
+    "D " +
+    hours +
+    " T" +
+    minutes +
+    "M " +
+    seconds +
+    "S ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById(
+      "demo"
+    ).innerHTML = "EXPIRED";
+  }
+}, 1000);
--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,10 @@ export default defineConfig({
     outDir: resolve("dist"),
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
+        main: resolve(
+          __dirname,
+          "src/index.html"
+        ),
         // about: resolve(__dirname, "src/about.html"),
       },
     },
